788a27a7565d1bfad1fa136185bf2a52701c939a
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-26 12:01:57 +0200

    Method Multiply. Make corrections in the code.

---
+++ TaskKyu0801/src/com/gmail/granovskiy/s/Multiply.java
@@ -0,0 +1,17 @@
+/*
+The code does not execute properly. Try to figure out why.
+Make corrections in the code.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Multiply {
+
+    public static void main(String[] args) {
+        System.out.println(multiply(5.12, 7.11));
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+}
